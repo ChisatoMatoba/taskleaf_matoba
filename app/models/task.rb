a57678b4ca1b01f009080ac869e89f2ec35d5d72
Chisato Matoba <chisato.matoba@luxiar.com>
2023-11-14 16:42:12 +0900

    コールバック処理を削除

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,6 @@
 
 # Represents task data as an ActiveRecord model.
 class Task < ApplicationRecord
-  before_validation :set_nameless_name
   validates :name, presence: true, length: { maximum: 30 }
   validate :validate_name_not_including_comma
 
@@ -10,9 +9,5 @@ class Task < ApplicationRecord
 
   def validate_name_not_including_comma
     errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
-  end
-
-  def set_nameless_name
-    self.name = '名前なし' if name.blank?
   end
 end
