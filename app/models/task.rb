c4eeb2f25c6f56156c5259457eb269b25a7dd2b5
Chisato Matoba <chisato.matoba@luxiar.com>
2023-11-14 15:23:34 +0900

    バリデーションエラー表示

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Represents task data as an ActiveRecord model.
 class Task < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence: true, length: { maximum: 30 }
 end
