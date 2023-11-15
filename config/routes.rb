bce5035d125de5046148193e4bff6d85e39faf6d
Chisato Matoba <chisato.matoba@luxiar.com>
2023-11-15 14:36:51 +0900

    軽微修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@
 Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  delete '/login', to: 'sessions#destroy'
+  delete '/logout', to: 'sessions#destroy'
+
   namespace :admin do
     resources :users
   end
