c3aa3a8b11c335976688fefeecc63e995370bb35
Chisato Matoba <chisato.matoba@luxiar.com>
2023-11-15 16:09:28 +0900

    タスクを新しい順に並べる(scopeで)

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,7 @@
 # TasksController handles the management of tasks in the application.
 class TasksController < ApplicationController
   def index
-    @tasks = current_user.tasks.order(created_at: :desc)
+    @tasks = current_user.tasks.recent
   end
 
   def show
